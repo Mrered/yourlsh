0406067c34a0436ec8f3b17721d1edfbc67a0b08
Mrered Cio <34948506+Mrered@users.noreply.github.com>
2023-07-08 09:46:55 +0800

    优化请求指令

--- yourlsh.sh
+++ yourlsh.sh
@@ -22,8 +22,5 @@ if [[ -z $url ]]; then
   exit 1
 fi
 
-# 执行 curl 请求并将结果保存到变量中
-result=$(curl -s -L "https://$domain/yourls-api.php?signature=$signature&action=shorturl&url=$url" | grep -o '<shorturl>[^<]*</shorturl>' | sed -e 's/<shorturl>\(.*\)<\/shorturl>/\1/')
-
-# 将结果复制到剪贴板
-echo "$result" | pbcopy
+# 执行 curl 请求并将结果复制到剪贴板
+echo -n $(curl -s -L "https://$domain/yourls-api.php?signature=$signature&action=shorturl&format=simple&url=$url") | pbcopy
