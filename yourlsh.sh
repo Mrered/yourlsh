130d8752aee007d73a0c76f71f1a605e9b3ccc2d
Mrered Cio <34948506+Mrered@users.noreply.github.com>
2023-07-09 21:06:03 +0800

    重构前的备份

--- yourlsh.sh
+++ yourlsh.sh
@@ -3,14 +3,24 @@
 CONFIG_DIR="$HOME/Library/Application Support/shurl"
 CONFIG_FILE="$CONFIG_DIR/config.json"
 
+# 方法 - 执行curl操作 
+curlFunc() {
+  curl -s -L "https://$1/yourls-api.php?signature=$2&action=shorturl&format=simple&url=$3"
+}
+
 # 获取剪贴板 url
 url=$(pbpaste)
+
+readConfig() {
+  jq -r ".$1" "$CONFIG_FILE"
+}
 
 # 判断是否需重新配置
 if [ "$1" == "-r" ] || [ ! -f "$CONFIG_FILE" ]; then
   if [ ! -d "$CONFIG_DIR" ]; then
     mkdir -p "$CONFIG_DIR"
   fi
+
   echo -n "输入你的域名，不包含 https:// （如：example.com）："
   read domain
   until [[ $domain == *"."* ]]; do
@@ -26,7 +36,7 @@ if [ "$1" == "-r" ] || [ ! -f "$CONFIG_FILE" ]; then
   # 写入配置文件
   echo "初始化成功，再次运行 shurl 即可正常使用，详细用法请访问 https://github.com/Mrered/yourlsh "
   url="https://images.unsplash.com/photo-1580855014124-d1e9d454c6b1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZXZlcnl0aGluZyUyMGlzJTIwZ29pbmclMjB0byUyMGJlJTIwYWxyaWdodHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
-  url=$(curl -s -L "https://$domain/yourls-api.php?signature=$signature&action=shorturl&format=simple&url=$url")
+  url=$(curlFunc $domain $signature $url)
   if [[ $url == "http"* ]]; then
     echo "{ \"domain\": \"$domain\", \"signature\": \"$signature\" }" > $CONFIG_FILE
     open "$url"
@@ -34,8 +44,7 @@ if [ "$1" == "-r" ] || [ ! -f "$CONFIG_FILE" ]; then
     echo "你的配置有误，请重新配置"
   fi
 else
-  domain=$(cat "$CONFIG_FILE" | awk -F":" '{print $2}' | awk -F"," '{print $1}'| sed 's/\"//g' | sed 's/ //g')
-  signature=$(cat "$CONFIG_FILE" | awk -F":" '{print $3}' | sed 's/\"//g' | sed 's/}//g' | sed 's/ //g')
-  # 执行 curl 操作
-  echo -n $(curl -s -L "https://$domain/yourls-api.php?signature=$signature&action=shorturl&format=simple&url=$url") | pbcopy
+  domain=$(readConfig domain)
+  signature=$(readConfig signature)
+  echo -n $(curlFunc $domain $signature $url) | pbcopy
 fi
