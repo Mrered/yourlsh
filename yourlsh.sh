4fe2dc3dd6a4e112d30eacee51906258ba10c57b
Mrered Cio <34948506+Mrered@users.noreply.github.com>
2023-07-09 20:15:24 +0800

    GPT 4 重构

--- yourlsh.sh
+++ yourlsh.sh
@@ -1,68 +1,37 @@
 #!/bin/zsh
-config_file=~/.shurl/config.json
+
+CONFIG_DIR="$HOME/Library/Application Support/shurl"
+CONFIG_FILE="$CONFIG_DIR/config.json"
+
+# 判断是否需重新配置
+if [ "$1" == "-r" ] || [ ! -f "$CONFIG_FILE" ]; then
+    if [ ! -d "$CONFIG_DIR" ]; then
+        mkdir -p "$CONFIG_DIR"
+    fi
+    echo -n "输入 domain (至少含有一个'.')："
+    read domain
+    until [[ $domain == *"."* ]]; do
+        echo "无效输入，请重新输入："
+        read domain
+    done
+
+    echo -n "输入 signature (数字/字母，10位)："
+    read signature
+    until [[ $signature =~ ^[0-9a-zA-Z]{10}$ ]]; do
+        echo "无效输入，请重新输入："
+        read signature
+    done
+
+    # 写入配置文件
+    echo "{ \"domain\": \"$domain\", \"signature\": \"$signature\" }" > $CONFIG_FILE
+    echo "设置成功！"
+else
+    domain=$(cat "$CONFIG_FILE" | awk -F":" '{print $2}' | awk -F"," '{print $1}'| sed 's/\"//g' | sed 's/ //g')
+    signature=$(cat "$CONFIG_FILE" | awk -F":" '{print $3}' | sed 's/\"//g' | sed 's/}//g' | sed 's/ //g')
+fi
+
+# 获取剪贴板 url
 url=$(pbpaste)
 
-function validate_domain {
-  local domain=$1
-  if [[ ! $domain =~ ^[a-zA-Z0-9.-]+$ ]]; then
-    echo "无效的域名，请重新输入正确的域名。"
-    return 1
-  fi
-}
-
-function validate_signature {
-  local signature=$1
-  if [[ ! $signature =~ ^[a-zA-Z0-9]{10}$ ]]; then
-    echo "无效的 signature token，请重新输入正确的值。"
-    return 1
-  fi
-}
-
-function read_configuration {
-  local data=$(cat "$config_file")
-  domain=$(echo "$data" | jq -r '.domain')
-  signature=$(echo "$data" | jq -r '.signature')
-
-  validate_domain "$domain" || return 1
-  validate_signature "$signature" || return 1
-}
-
-if [[ $1 == "-r" ]]; then
-  rm -f "$config_file"
-  echo "已删除配置文件 $config_file。"
-  echo "请输入以下参数："
-elif [[ -f $config_file ]]; then
-  read_configuration
-  if [[ $? -eq 0 ]]; then
-    echo "配置文件 $config_file 已存在，将使用其中的配置信息。"
-  else
-    echo "配置文件 $config_file 不完整或包含无效配置信息，请重新配置。"
-    exit 1
-  fi
-else
-  echo "配置文件 $config_file 不存在，请输入以下参数："
-  while true; do
-    read "domain?请输入域名，不包含 https:// （例如：example.com）: "
-    validate_domain "$domain" && break
-  done
-
-  while true; do
-    read "signature?请输入 signature token（十位数字字母组合）: "
-    validate_signature "$signature" && break
-  done
-
-  mkdir -p "$(dirname "$config_file")"  # 创建目录（如果不存在）
-  printf '{"domain":"%s","signature":"%s"}' "$domain" "$signature" > "$config_file"
-  echo "配置已保存到 $config_file。"
-fi
-
-if [[ -z $url ]]; then
-  echo "剪贴板中未找到URL，请复制URL然后再次执行脚本。"
-  exit 1
-fi
-
-# 执行 curl 请求并将结果保存到变量中
-result=$(curl -s -L "https://$domain/yourls-api.php?signature=$signature&action=shorturl&url=$url" | grep -o '<shorturl>[^<]*</shorturl>' | sed -e 's/<shorturl>\(.*\)<\/shorturl>/\1/')
-
-# 将结果复制到剪贴板
-echo "$result" | pbcopy
+# 执行 curl 操作
+echo -n $(curl -s -L "https://$domain/yourls-api.php?signature=$signature&action=shorturl&format=simple&url=$url") | pbcopy
