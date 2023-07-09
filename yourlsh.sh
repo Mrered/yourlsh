b8992d76023fc95b721b86c5de89fbb7d4872779
Mrered Cio <34948506+Mrered@users.noreply.github.com>
2023-07-09 21:15:00 +0800

    稳定版

--- yourlsh.sh
+++ yourlsh.sh
@@ -2,6 +2,7 @@
 
 CONFIG_DIR="$HOME/Library/Application Support/shurl"
 CONFIG_FILE="$CONFIG_DIR/config.json"
+result=""
 
 # 方法 - 执行curl操作 
 curlFunc() {
@@ -11,16 +12,34 @@ curlFunc() {
 # 获取剪贴板 url
 url=$(pbpaste)
 
-readConfig() {
-  jq -r ".$1" "$CONFIG_FILE"
+isCheck() {
+  jq -r ".${1}" "$CONFIG_FILE"
 }
 
-# 判断是否需重新配置
-if [ "$1" == "-r" ] || [ ! -f "$CONFIG_FILE" ]; then
+writeConfig() {
+  jq ". + {\"$1\": \"$2\"}" "$CONFIG_FILE" > tmp.$$.json && mv tmp.$$.json "$CONFIG_FILE"
+}
+
+# 在初始化之前检查是否需要重新配置
+needInitialization() {
+  if [ "$1" == "-r" ]; then
+    return 0
+  elif [ ! -f "$CONFIG_FILE" ]; then
+    return 0
+  elif ! jq -e '.domain and .signature and .check' "$CONFIG_FILE" >/dev/null 2>&1; then
+    return 0
+  elif [ "$(isCheck check)" == "false" ]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+initialization() {
   if [ ! -d "$CONFIG_DIR" ]; then
     mkdir -p "$CONFIG_DIR"
   fi
-
+    
   echo -n "输入你的域名，不包含 https:// （如：example.com）："
   read domain
   until [[ $domain == *"."* ]]; do
@@ -34,17 +53,26 @@ if [ "$1" == "-r" ] || [ ! -f "$CONFIG_FILE" ]; then
   done
 
   # 写入配置文件
-  echo "初始化成功，再次运行 shurl 即可正常使用，详细用法请访问 https://github.com/Mrered/yourlsh "
+  echo "{ \"domain\": \"$domain\", \"signature\": \"$signature\", \"check\": false }" > "$CONFIG_FILE"
+  echo "正在检测配置是否正确"
   url="https://images.unsplash.com/photo-1580855014124-d1e9d454c6b1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZXZlcnl0aGluZyUyMGlzJTIwZ29pbmclMjB0byUyMGJlJTIwYWxyaWdodHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
-  url=$(curlFunc $domain $signature $url)
-  if [[ $url == "http"* ]]; then
-    echo "{ \"domain\": \"$domain\", \"signature\": \"$signature\" }" > $CONFIG_FILE
-    open "$url"
+  result=$(curlFunc $domain $signature $url)
+  if [[ $result == "http"* ]]; then
+    writeConfig "check" "true"
+    open "$result"
+    echo "配置正确，初始化成功，详细用法请访问 https://github.com/Mrered/yourlsh "
   else
+    writeConfig "check" "false"
     echo "你的配置有误，请重新配置"
   fi
+}
+
+# 判断是否需重新配置
+if needInitialization "$1"; then
+  initialization
 else
-  domain=$(readConfig domain)
-  signature=$(readConfig signature)
-  echo -n $(curlFunc $domain $signature $url) | pbcopy
+  # 执行 curl 操作
+  domain=$(jq -r .domain "$CONFIG_FILE")
+  signature=$(jq -r .signature "$CONFIG_FILE")
+  echo -n $(curlFunc $domain $signature $url) | pbcopy 
 fi
